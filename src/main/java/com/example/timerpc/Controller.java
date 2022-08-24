b0358c35e8c91614dcd478876bb97a52565b6038
Bogdan <tatarchukbogdan80@gmail.com>
2022-08-24 12:36:19 +0200

    Second commit

--- src/main/java/com/example/timerpc/Controller.java
+++ src/main/java/com/example/timerpc/Controller.java
@@ -1,32 +1,66 @@
 package com.example.timerpc;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
 
-public class Controller {
+public class Controller{
 
-    private int seconds;
-    private int minutes;
-    private boolean isGoing;
-
+    Timeline clock;
+    int mil = 0;
+    int sec = 0;
+    int min = 0;
+    int h = 0;
 
     @FXML
     private Label time;
 
-
     @FXML
     void onPlay(ActionEvent event) {
-
+        clock = new Timeline(new KeyFrame(Duration.millis(1), actionEvent -> update(time)));
+        clock.setCycleCount(Timeline.INDEFINITE);
+        clock.setAutoReverse(false);
+        System.out.println("Begin");
+        clock.play();
     }
 
     @FXML
     void onReset(ActionEvent event) {
-
+        clock.stop();
+        mil = 0;
+        sec = 0;
+        min = 0;
+        h = 0;
+        time.setText("00:00:00:000");
     }
 
     @FXML
     void onStop(ActionEvent event) {
+        clock.stop();
+    }
 
+    void update(Label time){
+        if(mil == 1000){
+            mil = 0;
+            sec++;
+        }
+        if(sec == 60){
+            min++;
+            sec = 0;
+        }
+        if(min == 60){
+            h++;
+            min = 0;
+            sec = 0;
+            mil = 0;
+        }
+
+        time.setText(h + ":" + (((min/10) == 0) ? "0":"") + min + ":" + (((sec/10) == 0) ? "" : "") + sec + ":" +
+                (((mil/10) == 0) ? "80" : (((mil/100) == 0) ? "0" : "")) +mil++);
+        String s = h+":"+min+":"+sec+":"+mil;
+        System.out.println(s);
     }
 }
--- src/main/java/com/example/timerpc/Main.java
+++ src/main/java/com/example/timerpc/Main.java
@@ -7,14 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-import static javafx.application.Application.launch;
+public class Main extends Application{
 
-public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        stage.setTitle("Timer");
         stage.setScene(scene);
         stage.show();
     }
